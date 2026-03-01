a12e021d723db3a7769663bc690ccdb4be74343f
Leon Timmermans <fawaka@gmail.com>
2026-03-01 15:25:57 +0100

    Declare dependency on correct version of Future::IO

--- lib/Future/IO/Impl/Uring.pm
+++ lib/Future/IO/Impl/Uring.pm
@@ -7,7 +7,7 @@ use experimental 'signatures';
 use parent 'Future::IO::ImplBase';
 __PACKAGE__->APPLY;
 
-use Future::IO 0.019 qw/POLLIN POLLOUT/;
+use Future::IO 0.19 qw/POLLIN POLLOUT/;
 use IO::Uring 0.011 qw/IORING_TIMEOUT_ABS IORING_TIMEOUT_REALTIME IORING_TIMEOUT_ETIME_SUCCESS P_PID P_PGID P_ALL WEXITED/;
 use Errno 'ETIME';
 use Signal::Info qw/CLD_EXITED/;
